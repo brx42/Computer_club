6fd17c304d1ab6f8676f0553ee7921b7acb231db
brx42 <bruixot.p.r@gmail.com>
2022-07-21 16:57:36 +0300

    #3 Fix view Mapper

--- Computer_club.API/Mapper.cs
+++ Computer_club.API/Mapper.cs
@@ -10,29 +10,21 @@ public class Mapper : Profile
     public Mapper()
     {
         CreateMap<CreateUserCommand, User>().
-            ForMember(dest => dest.UserName, opt
-            => opt.MapFrom(src => src.Login)).
-            ForMember(dest => dest.PasswordHash, opt
-            => opt.MapFrom(src => src.Password));
+            ForMember(dest => dest.UserName,opt => opt.MapFrom(src => src.Login)).
+            ForMember(dest => dest.PasswordHash, opt => opt.MapFrom(src => src.Password));
         
         CreateMap<UpdateUserCommand, User>();
 
         CreateMap<User, CreateUserResult>().
-            ForMember(dest => dest.Login, opt 
-            => opt.MapFrom(src => src.UserName)).
-            ForMember(dest => dest.Password, opt 
-            => opt.MapFrom(src => src.PasswordHash));
+            ForMember(dest => dest.Login, opt => opt.MapFrom(src => src.UserName)).
+            ForMember(dest => dest.Password, opt => opt.MapFrom(src => src.PasswordHash));
         
         CreateMap<User, GetByIdUserResult>().
-            ForMember(dest => dest.Login, opt
-            => opt.MapFrom(src => src.UserName)).
-            ForMember(dest => dest.Password, opt 
-            => opt.MapFrom(src => src.PasswordHash));
+            ForMember(dest => dest.Login, opt => opt.MapFrom(src => src.UserName)).
+            ForMember(dest => dest.Password, opt => opt.MapFrom(src => src.PasswordHash));
         
         CreateMap<User, UpdateUserResult>().
-            ForMember(dest => dest.Login, opt
-            => opt.MapFrom(src => src.UserName)).
-            ForMember(dest => dest.Password, opt
-            => opt.MapFrom(src => src.PasswordHash));
+            ForMember(dest => dest.Login, opt => opt.MapFrom(src => src.UserName)).
+            ForMember(dest => dest.Password, opt => opt.MapFrom(src => src.PasswordHash));
     }
 }
